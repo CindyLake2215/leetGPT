6245f23792d0901e56aed0adac87b35ffa12e09a
CindyLake2215 <cindy.lake2215@gmail.com>
2024-12-02 17:26:57 +0000

    updated 179

--- 179/Solution.java
+++ 179/Solution.java
@@ -1,14 +1,13 @@
 import java.util.Arrays;
-import java.util.Comparator;
 
 class Solution {
     public String largestNumber(int[] nums) {
-        // Convert integers to strings
+        // Convert numbers to strings
         String[] numStrings = Arrays.stream(nums)
                                     .mapToObj(String::valueOf)
                                     .toArray(String[]::new);
 
-        // Sort strings using a custom comparator
+        // Sort using custom comparator
         Arrays.sort(numStrings, (a, b) -> (b + a).compareTo(a + b));
 
         // If the largest number is "0", return "0"
@@ -16,7 +15,7 @@ class Solution {
             return "0";
         }
 
-        // Join sorted strings into a single result
+        // Join sorted strings to form the largest number
         return String.join("", numStrings);
     }
 
@@ -25,14 +24,22 @@ class Solution {
 
         // Test case 1
         int[] nums1 = {10, 2};
-        System.out.println("Input: " + Arrays.toString(nums1) + " -> Output: " + solution.largestNumber(nums1));
+        System.out.println("Input: " + Arrays.toString(nums1) + " -> Output: " + solution.largestNumber(nums1)); // Expected: "210"
 
         // Test case 2
         int[] nums2 = {3, 30, 34, 5, 9};
-        System.out.println("Input: " + Arrays.toString(nums2) + " -> Output: " + solution.largestNumber(nums2));
+        System.out.println("Input: " + Arrays.toString(nums2) + " -> Output: " + solution.largestNumber(nums2)); // Expected: "9534330"
 
         // Test case 3
-        int[] nums3 = {0, 0};
-        System.out.println("Input: " + Arrays.toString(nums3) + " -> Output: " + solution.largestNumber(nums3));
+        int[] nums3 = {1};
+        System.out.println("Input: " + Arrays.toString(nums3) + " -> Output: " + solution.largestNumber(nums3)); // Expected: "1"
+
+        // Test case 4
+        int[] nums4 = {10};
+        System.out.println("Input: " + Arrays.toString(nums4) + " -> Output: " + solution.largestNumber(nums4)); // Expected: "10"
+
+        // Test case 5
+        int[] nums5 = {0, 0};
+        System.out.println("Input: " + Arrays.toString(nums5) + " -> Output: " + solution.largestNumber(nums5)); // Expected: "0"
     }
 }
